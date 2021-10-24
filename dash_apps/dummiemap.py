23f6720e6f586cd210cc98bcd761e952c11b7193
Raul De la Rosa <raul@entropia.ai>
2021-10-24 12:47:42 -0500

    geolocation control added->change to leaflet api

--- dash_apps/dummiemap.py
+++ dash_apps/dummiemap.py
@@ -1,6 +1,8 @@
 import dash_html_components as html
 import plotly.graph_objects as go
 import dash_core_components as dcc
+import dash_leaflet as dl
+
 import server
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
@@ -24,6 +26,7 @@ def create_layout():
         type='circle', fullscreen=True,
         loading_state={'is_loading': True},
         style={'backgroundColor': '#FFFFFF00'})
+
     return layout
 
 
@@ -31,34 +34,25 @@ def create_layout():
     Output("graph-map", "children"), 
     [Input("dropdown-map_update", "value")])
 def update_bar_chart(value):
-    mapbox_center_lon=-99.1817107
-    mapbox_center_lat=19.3268706
+    url = "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
+    attribution = "<a href=https://www.google.com/maps/@24,-101.4,3274426m/data=!3m1!1e3/>Google Maps</a>"
 
-  
-    fig = go.Figure(go.Scattermapbox(
-        lat=[],
-        lon=[],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=14
-        ),
-        text=['Montreal'],
-    ))
-    fig.update_layout(mapbox_zoom=13,
-                        mapbox_style="open-street-map",
-                        mapbox_center_lon=mapbox_center_lon,
-                        mapbox_center_lat=mapbox_center_lat)
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})            
-    layout = html.Div([dcc.Graph(figure=fig, 
-                                    config={'displayModeBar': False})])
+    fig = dl.Map([dl.TileLayer(url=url, maxZoom=20, attribution=attribution), 
+                 dl.LocateControl(options={'locateOptions': {'enableHighAccuracy': True}})],
+                 id="map", style={'width': '100%', 'height': '480px', 'margin': "auto", "display": "block"},
+                 zoom=13.5, center=(19.32,-99.186)
+                 )
+
+    layout = html.Div([fig,html.Div(id="text")])
 
     return layout
 
 
-
-
-
-
-
-
-
+@server.app_dash.callback(Output("text", "children"), [Input("map", "location_lat_lon_acc")])
+def update_location(location):
+    if location:
+        lat,lon = location[0], location[1]
+        #print(lat,lon)
+        #return 
+    else:
+     pass
--- server.py
+++ server.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 app_dash = dash.Dash(__name__, server=app,
             meta_tags=[{"name": "viewport", "content":
                         "width=device-width, initial-scale=1"}],
-            url_base_pathname="/dash/")
+            url_base_pathname="/dash/",
+            external_stylesheets=['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
+            #prevent_initial_callbacks=True
+            )
     
 app_dash.config.suppress_callback_exceptions = True
 app_dash.title = 'Proyecto'
