fbf5c59ad6d5f9fbec4b4a1c0632b7520b1de24b
Raúl De la Rosa <52948367+rulas99@users.noreply.github.com>
2022-01-04 23:44:55 -0600

    Update here_api.py

--- dash_apps/auxiliar_functions/here_api.py
+++ dash_apps/auxiliar_functions/here_api.py
@@ -7,7 +7,7 @@ def hereRequestRoutes(origen,destino):
     destino = ','.join([str(i) for i in destino])
     
     URL = "https://router.hereapi.com/v8/routes"
-    api_key = 't5feKE8N0lDxz3GCoBJ9A_s-9YSr9Y-aCc3NJbvsvEk' # Acquire from developer.here.com
+    api_key = '<token>' # Acquire from developer.here.com
     PARAMS = {'apikey':api_key,'transportMode':'pedestrian','return':'polyline','destination':destino,'origin':origen} 
     
     r = get(url = URL, params = PARAMS) 
